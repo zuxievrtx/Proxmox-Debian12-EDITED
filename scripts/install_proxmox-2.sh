ba1967bf48ca5ff6da34bc3c8476fdaa8079f15f
Zuxiecxz <125678959+zuxievrtx@users.noreply.github.com>
2025-08-20 00:24:00 +0700

    Update install_proxmox-2.sh

--- scripts/install_proxmox-2.sh
+++ scripts/install_proxmox-2.sh
@@ -6,8 +6,8 @@
 cd /Proxmox-Debian12
 
 # Load configs files // Carregar os arquivos de configuração
-source ./configs/colors.conf
-source ./configs/language.conf
+source ../configs/colors.conf
+source ../configs/language.conf
 
 # Becoming superuser // Tornando-se superusuário
 super_user()
